6e0557f9aae4dfa8b892b73bb5770c965b472729
vagne <vagnerdw1@gmail.com>
2024-11-18 10:45:34 -0300

    Corrige erro na configuração do RabbitMQ e na publicação de eventos de usuário

--- src/main/java/com/ead/authuser/publishers/UserEventPublisher.java
+++ src/main/java/com/ead/authuser/publishers/UserEventPublisher.java
@@ -1,11 +1,9 @@
 package com.ead.authuser.publishers;
-
 import com.ead.authuser.dtos.UserEventDto;
 import com.ead.authuser.enums.ActionType;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Component
